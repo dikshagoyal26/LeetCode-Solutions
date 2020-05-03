2619b92f4fe7a937ca98605328d186fc19bee5a7
diksha goyal <diksha.goyal@intelliticks.com>
2020-05-03 23:59:17 +0530

    added ques

---
+++ top-interview-questions/array/  remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,25 @@
+/** 
+Title :  Remove Duplicates from Sorted Array
+Link  :  https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+Idea  :  Once an element is encountered, bypass its duplicates and move on to the next unique element.
+Time Complexity : O(n)
+*/
+
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int c = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[c++] = nums[i];
+            while (i < nums.size() - 1 && nums[i + 1] == nums[i])
+            {
+                i++;
+            }
+        }
+        return c;
+    }
+};
