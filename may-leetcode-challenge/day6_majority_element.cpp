a42ce4d60202229056e6bf6a84ac65b56ba6beb4
diksha goyal <diksha.goyal@intelliticks.com>
2020-05-08 22:59:28 +0530

    update day6_majority_element.cpp

--- may-leetcode-challenge/day6_majority_element.cpp
+++ may-leetcode-challenge/day6_majority_element.cpp
@@ -4,18 +4,22 @@ Link  :  https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/53
 Idea  :  Creating a count a storing the element, if count is zero then updating the count with curr element and then checking if majElement is equal to curr element then increment the count else decrement it
 Time Complexity : O(N) 
 */
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var majorityElement = function (nums) {
-    var count = 0;
-    var majElement;
-    for (let i = 0; i < nums.length; i++) {
-        if (count == 0) {
-            majElement = nums[i];
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0;
+        int majElement;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (count == 0)
+            {
+                majElement = nums[i];
+            }
+            count += (majElement == nums[i]) ? 1 : -1;
         }
-        count += (majElement == nums[i]) ? 1 : -1;
+        return majElement;
     }
-    return majElement;
 };
