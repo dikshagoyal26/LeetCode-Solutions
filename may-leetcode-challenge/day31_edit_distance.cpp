534ced39e70f802feb9c72adc5cf31706be38ebb
diksha goyal <diksha.goyal@intelliticks.com>
2020-05-31 13:10:31 +0530

    🎉 completing the challenge

---
+++ may-leetcode-challenge/day31_edit_distance.cpp
@@ -0,0 +1,34 @@
+/** 
+Title :   Edit Distance
+Link  :   https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/538/week-5-may-29th-may-31st/3346/
+Idea  :   Dynamic Programming
+Time Complexity : O(nm)
+*/
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        int m = word1.length(), n = word2.length();
+        int dp[m + 1][n + 1];
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
